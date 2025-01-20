3bdb834aec0393587b330d3981b88484d481f0dc
Lohran Mendes <lohranfm@gmail.com>
2025-01-20 01:23:07 -0300

    feat: add Create-thought component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { CreateThoughtComponent } from "./components/thoughts/create-thought/create-thought.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, CreateThoughtComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
