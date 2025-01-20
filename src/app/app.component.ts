24edeef7e94651fdfb160046a26be3a2c9350298
Lohran Mendes <lohranfm@gmail.com>
2025-01-20 00:47:23 -0300

    feat: add Footer component - finish background of home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'memoteca-v2';
 }
