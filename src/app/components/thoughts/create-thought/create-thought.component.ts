d912dd47f8eab4276996de8b632e89d93aa6f195
Lohran Mendes <lohranfm@gmail.com>
2025-01-20 01:23:07 -0300

    feat: add Create-thought component

---
+++ src/app/components/thoughts/create-thought/create-thought.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-thought',
+  imports: [],
+  templateUrl: './create-thought.component.html',
+  styleUrl: './create-thought.component.css'
+})
+export class CreateThoughtComponent {
+
+}
