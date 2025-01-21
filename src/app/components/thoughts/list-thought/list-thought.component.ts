8313a52e42841df79a1123a640840cc3bcbb0135
Lohran Mendes <lohranfm@gmail.com>
2025-01-21 10:13:37 -0300

    feat(list-thought): add List-thought component

---
+++ src/app/components/thoughts/list-thought/list-thought.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-thought',
+  imports: [],
+  templateUrl: './list-thought.component.html',
+  styleUrl: './list-thought.component.css'
+})
+export class ListThoughtComponent {
+
+}
