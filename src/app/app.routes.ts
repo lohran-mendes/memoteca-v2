8e33e97d7ad6183b42c8ca4a8fca951defd21fc9
Lohran Mendes <lohranfm@gmail.com>
2025-02-04 20:34:24 -0300

    feat(thought): add Thought service - Create Thought service - Add routes used in Thought service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { CreateThoughtComponent } from './components/thoughts/create-thought/create-thought.component';
 import { ListThoughtComponent } from './components/thoughts/list-thought/list-thought.component';
+import { DeleteThoughtComponent } from './components/thoughts/delete-thought/delete-thought.component';
 
 export const routes: Routes = [
   {
@@ -17,4 +18,12 @@ export const routes: Routes = [
     path: 'listar-pensamentos',
     component: ListThoughtComponent,
   },
+  {
+    path: 'pensamentos/deletar-pensamentos/:id',
+    component: DeleteThoughtComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'criar-pensamentos'
+  }
 ];
