a6d3b7c5b073992bec5114f30481665d8e700156
Lohran Mendes <lohranfm@gmail.com>
2025-02-04 20:44:32 -0300

    feat(delete-thought): add Delete-thought component

---
+++ src/app/components/thoughts/delete-thought/delete-thought.component.ts
@@ -0,0 +1,48 @@
+import { Component, Inject, inject, OnInit, signal } from '@angular/core';
+import { type Thougth } from '../../../interfaces/thought';
+import { ThoughtService } from '../../../services/thought.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-delete-thought',
+  templateUrl: './delete-thought.component.html',
+  styleUrl: './delete-thought.component.css',
+})
+export class DeleteThoughtComponent implements OnInit {
+  thoughtService = inject(ThoughtService);
+  router = inject(Router);
+  route = inject(ActivatedRoute);
+
+  thought = signal<Thougth>({
+    autoria: '',
+    conteudo: '',
+    modelo: '',
+    id: 0,
+  });
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    if(id){
+      const numericId = Number(id)
+      this.thoughtService.searchThought(numericId).subscribe({
+        next: (retorno) =>
+          this.thought.update((currentValue) => ({
+            ...currentValue,
+            id: retorno.id,
+          })),
+        });
+      }
+  }
+
+  excluirPensamento() {
+    const thoughtId = this.thought().id;
+    if (thoughtId) {
+      this.thoughtService.deleteThought(thoughtId).subscribe({
+        next: () => this.router.navigate(['listar-pensamentos']),
+      });
+    }
+  }
+  cancelar() {
+    this.router.navigate(['listar-pensamentos']);
+  }
+}
